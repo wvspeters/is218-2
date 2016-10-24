e2628a5c00ab2158cea6b75a4cc0187dd513ad05
wp39 <wp39@njit.edu>
2016-10-24 11:59:24 -0400

    Array Walk

--- examples/array.php
+++ examples/array.php
@@ -2,4 +2,9 @@
 	$a=array("Fruits"=>"Apple","Vegtables"=>"Broccoli","Milk"=>"Whole");
 	print_r(array_keys($a));
 	echo array_search("Apple",$a);
+
+	function example_funct($value,$key){
+		echo "$key with a value of $value<br>";
+	}
+	array_walk($a, "example_funct")
 ?>
